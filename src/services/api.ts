4176d1b64063644462f0079c11c092f65eaa88f4
EeroVallistu <eero.vallistu@vikk.ee>
2025-03-07 17:07:24 +0200

    Add setupProxy.js for dev enviroment

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: '/api',  // Changed from http://localhost:3000 to /api
+    baseURL: '/api',  
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
